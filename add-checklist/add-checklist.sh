7c22d4aedf76be78669ae2a3d3206d22b46ea849
Rich Knight <rich@rich-knight.com>
2019-03-06 15:07:32 +0000

    Fix event path

--- add-checklist/add-checklist.sh
+++ add-checklist/add-checklist.sh
@@ -7,7 +7,7 @@ API_VERSION=v3
 API_HEADER="Accept: application/vnd.github.${API_VERSION}+json; application/vnd.github.antiope-preview+json"
 AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 
-COMMENTS_URL=`jq -r .pull_request._links.comments.href sample.json`
+COMMENTS_URL=`jq -r .pull_request._links.comments.href "$GITHUB_EVENT_PATH"`
 echo "Posting comment..."
 curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" -d '{ "body": "hello world" }' -H "Content-Type: application/json" -X POST "$COMMENTS_URL"
 echo "Done"
