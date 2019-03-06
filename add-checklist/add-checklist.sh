ae008cbe634b21646a759f86639dad7fd248a068
Rich Knight <rich@rich-knight.com>
2019-03-06 15:03:44 +0000

    Updated script to post comment

--- add-checklist/add-checklist.sh
+++ add-checklist/add-checklist.sh
@@ -1,8 +1,13 @@
-
 #!/bin/bash
 set -e
 set -o pipefail
-main() {
-   jq --raw-output . "$GITHUB_EVENT_PATH"
-}
-main
+
+URI=https://api.github.com
+API_VERSION=v3
+API_HEADER="Accept: application/vnd.github.${API_VERSION}+json; application/vnd.github.antiope-preview+json"
+AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
+
+COMMENTS_URL=`jq -r .pull_request._links.comments.href sample.json`
+echo "Posting comment..."
+curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" -d '{ "body": "hello world" }' -H "Content-Type: application/json" -X POST "$COMMENTS_URL"
+echo "Done"
