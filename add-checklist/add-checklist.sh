9134cf30f3ace20a78463f2745bcf9cc980e3481
Rich Knight <rich@rich-knight.com>
2019-03-06 15:27:30 +0000

    Post templated json

--- add-checklist/add-checklist.sh
+++ add-checklist/add-checklist.sh
@@ -8,6 +8,9 @@ API_HEADER="Accept: application/vnd.github.${API_VERSION}+json; application/vnd.
 AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 
 COMMENTS_URL=`jq -r .pull_request._links.comments.href "$GITHUB_EVENT_PATH"`
+echo "Building comment body..."
+jq -R -s --argfile json /usr/bin/postTemplate.json -f /usr/bin/jqprogram.jq /usr/bin/checklist.md > ./commentBody.json 
+cat ./commentBody.json
 echo "Posting comment..."
-curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" -d '{ "body": "hello world" }' -H "Content-Type: application/json" -X POST "$COMMENTS_URL"
+curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" -d @commentBody.json -H "Content-Type: application/json" -X POST "$COMMENTS_URL"
 echo "Done"
